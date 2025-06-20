d69668dfd586876bebeb8ee7b438744846ead43f
babu160487 <jayazh87@gmail.com>
2025-06-21 04:58:59 +0530

    Few lines added by Babu

---
+++ src/org/git/JavaGit2025.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class JavaGit2025 {
+	
+	public static void main(String[] args) {
+	
+		int a = 10;
+		System.out.println(a);
+		
+		String s = "12345";
+		s.replaceAll("[1-5]", "a-e");
+		System.out.println(s);
+			
+	}
+}
